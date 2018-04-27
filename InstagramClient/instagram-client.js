8e89bc7159e0c56676564dd5706461f1cf4814bf
Ofir Ben-Ezra <ofir.benezra@ruckuswireless.com>
2018-04-27 16:21:09 +0300

    First commit

---
+++ InstagramClient/instagram-client.js
@@ -0,0 +1,56 @@
+const https = require("https");
+
+var images = [];
+var tag = "";
+var token = '7484048701.40116e0.346f60d6b2694139b646173871cef740',
+    num_photos = 1; // maximum 20
+
+const url =
+    "https://api.instagram.com/v1/tags/stmobileapps/media/recent?&access_token=" + token;
+
+function recursiveGetProp(obj, lookup, callback) {
+    for (property in obj) {
+        if (property == lookup) {
+            callback(obj[property]);
+        } else if (obj[property] instanceof Object) {
+            recursiveGetProp(obj[property], lookup, callback);
+        }
+    }
+}
+var InstaClient = {
+    getProps(){
+        // Return new promise
+        return new Promise(function(resolve, reject) {
+            // Do async job
+            https.get(url, res => {
+                res.setEncoding("utf8");
+                var body = "";
+
+                res.on("data", data => {
+                    body += data;
+
+                });
+
+                res.on("end", () => {
+                    console.log(body);
+                    const jsonObj = JSON.parse(body);
+
+                    const ImageData = jsonObj.data.map((obj) => {
+                            return {
+                                url:obj.images.standard_resolution.url,
+                                width:obj.images.standard_resolution.width,
+                                height:obj.images.standard_resolution.height,
+                            };
+                        });
+                    console.log(ImageData);
+                    resolve(ImageData);
+                });
+            });
+        })
+
+
+
+
+    }
+};
+module.exports = InstaClient;
---
+++ routes/api.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var InstagramClient  = require('../InstagramClient/instagram-client')
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    let promiseObj = InstagramClient.getProps().then((body) => {
+        res.json(body);
+    });
+});
+
+module.exports = router;
